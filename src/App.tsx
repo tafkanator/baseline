dfe708db3a03b7b4a64aae7b3f9785aaf99ae5f3
Taavi Sangel <taavi.sangel@outlook.com>
2015-09-16 21:16:15 +0300

    added typescript, react and webpack

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface props {}
+interface state {}
+
+export class App extends React.Component<props, state> {
+	render() {
+		return <h2>tere</h2>;
+	}
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+/// <reference path="../typings/react/react.d.ts" />
+ 
+import * as React from 'react';
+
+import { App } from './App.tsx';
+
+React.render(<App />, document.getElementById('root'));
